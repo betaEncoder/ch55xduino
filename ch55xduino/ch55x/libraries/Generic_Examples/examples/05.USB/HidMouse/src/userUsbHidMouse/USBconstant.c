05b6ddb5a15d1bfc36847ec934a24dbac0f9fd91
betaEncoder <betaencoder@gmail.com>
2021-05-30 00:27:56 +0900

    fix serial string descriptor

--- ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/HidMouse/src/userUsbHidMouse/USBconstant.c
+++ ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/HidMouse/src/userUsbHidMouse/USBconstant.c
@@ -95,8 +95,8 @@ __code uint16_t CfgDescLen = sizeof(CfgDesc);
 __code uint8_t LangDes[]={0x04,0x03,0x09,0x04};           //Language Descriptor
 __code uint16_t LangDesLen = sizeof(LangDes);
 __code uint8_t SerDes[]={                                 //Serial String Descriptor
-    0x14,0x03,
-    'C',0x00,'H',0x00,'5',0x00,'5',0x00,'x',0x00,' ',0x00,'k',0x00,'b',0x00,'d',0x00
+    0x18,0x03,
+    'C',0x00,'H',0x00,'5',0x00,'5',0x00,'x',0x00,' ',0x00,'m',0x00,'o',0x00,'u',0x00,'s',0x00,'e',0x00
 };
 __code uint16_t SerDesLen = sizeof(SerDes);
 __code uint8_t Prod_Des[]={                                //Produce String Descriptor
